12ed7be64c9e30edc3c674642ebbbefa8d6c3931
nokkvikarlsson <nokkvi17@ru.is>
2019-01-23 14:27:30 +0000

    created state class

---
+++ prog1/src/State.java
@@ -0,0 +1,22 @@
+import java.util.Collection;
+
+/* State (class or an object):
+	Posx
+	Posy
+	On
+	Orientation
+	DirtsCleaned
+	availableMoves() //if on dirt, only move avaible is suck (saves memory by reducing         possible branches)
+	execute(MOVE)
+*/
+
+public class State
+{
+    public int Posx;
+    public int Posy;
+    public boolean On;
+    public int Orientation; //1 = NORTH, 2 = EAST, 3 = SOUTH, 4 = WEST
+    public int DirtsCleaned;
+    //Available moves
+    //public void execute(MOVE)
+}
