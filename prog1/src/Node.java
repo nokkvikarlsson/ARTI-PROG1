074e2a9dff13d765222b33d0838654ec15b21cb4
Kalman Laptop <sigurdur.kalman@hotmail.com>
2019-01-24 18:44:53 +0000

    Old structure restored to retrieve lost code

---
+++ prog1/src/Node.java
@@ -0,0 +1,18 @@
+import java.util.Collection;
+
+/* State (class or an object):
+	Posx
+	Posy
+	On
+	Orientation
+	DirtsCleaned
+	availableMoves() //if on dirt, only move avaible is suck (saves memory by reducing         possible branches)
+	execute(MOVE)
+*/
+
+public class Node
+{
+    public State state; 
+    //available moves()
+    //public void execute(MOVE)
+}
